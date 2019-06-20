10d97b609b159fb4318c8f3681cf1f857fa34ede
Humberto Alfonso Díaz <humberto.alfonso@asvito.es>
2019-06-20 13:08:08 +0200

    DOC Add code comment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
+/* JS */
 import '@iconfu/svg-inject';
 import './js/AFB.js';
 import './js/app.js';
-
-console.log('Arrancada la aplicación compilando CSS y SaSS');
-
 
 /* CSS */
 import './styles/app.scss';
