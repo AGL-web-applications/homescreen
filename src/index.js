d15f8cb06f8d45e9bdaf28cf861f2f14d23ef968
Humberto Alfonso Díaz <humberto.alfonso@asvito.es>
2019-06-21 11:00:42 +0200

    FUNCT Basic support to launch apps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 /* JS */
 import '@iconfu/svg-inject';
 import './js/AFB.js';
-import './js/app.js';
+import { init, launch } from './js/app.js';
 
 /* CSS */
 import './styles/app.scss';
+
+window.launch = launch;
+
+document.addEventListener('DOMContentLoaded', init);
