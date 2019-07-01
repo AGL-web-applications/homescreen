954db2a44b1eab1c973bd94382f5abc5829f7b1e
Humberto Alfonso Díaz <humberto.alfonso@asvito.es>
2019-07-01 13:28:37 +0200

    RESTRUCT Update layout to remove homescreen styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 /* JS */
-import '@iconfu/svg-inject';
 import './js/AFB.js';
 import { init, launch } from './js/app.js';
 
 /* CSS */
 import './styles/app.scss';
-
-window.launch = launch;
 
 document.addEventListener('DOMContentLoaded', init);
